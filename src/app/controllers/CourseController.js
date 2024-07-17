b220a1e60395d8e03c5df33e1345b3f77e674023
FinoDev <kieuvantrung.dev@gmail.com>
2024-07-17 18:45:21 +0700

    Update course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -49,6 +49,21 @@ class CourseController {
       .then(() => res.redirect("/"))
       .catch((error) => {});
   }
+
+  //[GET] /courses/:id/edit
+  edit(req, res, next) {
+    Course.findById(req.params.id)
+      .lean()
+      .then((course) => res.render("courses/edit", { course }))
+      .catch(next);
+  }
+
+  //[PUT] /courses/:id/update
+  update(req, res, next) {
+    Course.updateOne({ _id: req.params.id }, req.body)
+      .then(() => res.redirect("/me/stored/courses"))
+      .catch(next);
+  }
 }
 
 module.exports = new CourseController();
--- src/server.js
+++ src/server.js
@@ -1,17 +1,18 @@
 //import library
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
+const methodOverride = require("method-override");
 
 //use library
 const app = express();
 const port = 8080;
 
 //import src
-const configViewEngine = require('./config/viewEngine');
-const route = require('./routes');
+const configViewEngine = require("./config/viewEngine");
+const route = require("./routes");
 
 //import connect mongoose
-const db = require('./config/db');
+const db = require("./config/db");
 
 //Connect to DB
 db.connect();
@@ -20,8 +21,10 @@ db.connect();
 app.use(express.urlencoded({ extended: true })); //form
 app.use(express.json()); //javascript
 
+app.use(methodOverride("_method"));
+
 //Morgan --HTTP logger
-app.use(morgan('short'));
+app.use(morgan("short"));
 
 //express-handlebars --Template engine
 configViewEngine(app, express);
