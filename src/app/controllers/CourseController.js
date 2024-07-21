fb6bfec9beceb04b22567c5b37f1310719029aea
FinoDev <kieuvantrung.dev@gmail.com>
2024-07-21 22:21:32 +0700

    Add Soft Delete

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -64,6 +64,20 @@ class CourseController {
       .then(() => res.redirect("/me/stored/courses"))
       .catch(next);
   }
+
+  //[DELETE] /courses/:id/delete
+  destroy(req, res, next) {
+    Course.delete({_id: req.params.id})
+      .then(() => res.redirect("back"))
+      .catch(next);
+  }
+
+  //[PATCH] /courses/:id/delete
+  restore(req, res, next) {
+    Course.restore({_id: req.params.id})
+      .then(() => res.redirect("back"))
+      .catch(next);
+  }
 }
 
 module.exports = new CourseController();
--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -3,18 +3,47 @@ const { mongooseToObject } = require("../../util/mongoose");
 
 class MeController {
   //[GET] /me/stored/courses
-  async storedCourses(req, res, next) {
-    try {
-      const courses = await Course.find({}).lean();
-      res.render("me/stored-courses", {
-        courses,
-        helpers: {
-          sum: (a, b) => a + b,
-        },
-      });
-    } catch (err) {
-      next(err);
-    }
+  // async storedCourses(req, res, next) {
+  //   try {
+  //     const courses = await Course.find({}).lean();
+  //     res.render("me/stored-courses", {
+  //       courses,
+  //       helpers: {
+  //         sum: (a, b) => a + b,
+  //       },
+  //     });
+  //   } catch (err) {
+  //     next(err);
+  //   }
+  // }
+
+  storedCourses(req, res, next) {
+    Course.find({})
+      .lean()
+      .then((courses) =>
+        res.render("me/stored-courses", {
+          courses,
+          helpers: {
+            sum: (a, b) => a + b,
+          },
+        })
+      )
+      .catch(next);
+  }
+
+  //[GET] /me/trash/courses
+  trashCourses(req, res, next) {
+    Course.findWithDeleted({ deleted: true })
+      .lean()
+      .then((courses) =>
+        res.render("me/trash-courses", {
+          courses,
+          helpers: {
+            sum: (a, b) => a + b,
+          },
+        })
+      )
+      .catch(next);
   }
 }
 
